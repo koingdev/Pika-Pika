a2432d7983f2ee4d4ce3be9d76df67b261e0d3e3
Saingkoing SEA <sea.saingkoing@gmail.com>
2022-07-29 20:46:43 +0900

    Initial Commit

---
+++ Pika Pika/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pika Pika
+//
+//  Created by KoingDev on 29/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
